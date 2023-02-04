9b1d2045432f186c581676a8dda161bbd4a4048b
AlifSakib <alifsakib@gmail.com>
2023-02-05 00:56:06 +0600

    chartjs barchart complete

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import { BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Tooltip } from "chart.js";
 import { useRef } from 'react';
-import { Bar } from "react-chartjs-2";
+import { Bar, getElementAtEvent } from "react-chartjs-2";
 
 ChartJS.register(BarElement, CategoryScale, LinearScale, Tooltip, Legend);
 
@@ -17,13 +17,25 @@ function App() {
         borderColor: "black",
         backgroundColor: ["aqua", "red", "aqua", "red"],
         borderWidth: 1,
+        links: [
+          "https:/www.chartjs3.com",
+          "https:/www.chartjs3.com",
+          "https:/www.chartjs3.com",
+          "https:/www.chartjs3.com",
+        ],
       },
     ],
   };
 
   const chartRef = useRef()
   const onClick = (e) => {
-    console.log(e)
+    
+    if (getElementAtEvent(chartRef.current, e).length > 0){
+      console.log(getElementAtEvent(chartRef.current, e)[0]);
+      const dataSetIndexNum = getElementAtEvent(chartRef.current, e)[0].datasetIndex
+      const dataPoint = getElementAtEvent(chartRef.current, e)[0].index
+      window.open(data.datasets[dataSetIndexNum].links[dataPoint], "_blank")
+    }
   }
   const options = {}
   return (
